dc36de7df502b58158690ce2e8cc8b81fefa2364
shots47s <stbrown@psc.edu>
2021-09-30 07:58:19 -0400

    adding density test

--- tests/SCF/test_SCF.py
+++ tests/SCF/test_SCF.py
@@ -6,3 +6,14 @@ def test_calc_nuclear_repulsion_energy(mol_h2o):
     assert True
 #    assert SCF.calc_nuclear_repulsion_energy(mol_h2o) == 8.00236706181077,\
 #        "Nuclear Repulsion Energy Test (H2O) Failed"
+
+
+def test_calc_initial_density(mol_h2o):
+    """
+    Tests that the initial density returns a zero matrix
+    and tests dimensions
+    """
+
+    Duv = SCF.calc_initial_density(mol_h2o)
+    assert Duv.sum() == 0.0
+    assert Duv.shape == (mol_h2o.nao, mol_h2o.nao)
